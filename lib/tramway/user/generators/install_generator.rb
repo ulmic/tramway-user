18f47ff87703b69354ecf4a6f0add52ae01779e4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-11-27 00:10:44 +0400

    Add inheritance to Core install_generator

--- lib/tramway/user/generators/install_generator.rb
+++ lib/tramway/user/generators/install_generator.rb
@@ -1,7 +1,8 @@
 require 'rails/generators'
+require 'tramway/core/generators/install_generator'
 
 module Tramway::User::Generators
-  class InstallGenerator < ::Rails::Generators::Base
+  class InstallGenerator < ::Tramway::Core::Generators::InstallGenerator
     include Rails::Generators::Migration
     source_root File.expand_path('../templates', __FILE__)
 
@@ -9,11 +10,6 @@ module Tramway::User::Generators
 
     def run_another_generators
       generate 'tramway:core:install'
-    end
-
-    def self.next_migration_number(path)
-      next_migration_number = current_migration_number(path) + 1
-      ActiveRecord::Migration.next_migration_number next_migration_number
     end
 
     def copy_migrations
